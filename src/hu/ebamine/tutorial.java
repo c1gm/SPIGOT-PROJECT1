b1399bbbe719e4dfc5319b9cdf6e19c6ee66c7a1
c1gm <63867002+c1gm@users.noreply.github.com>
2020-04-22 17:03:05 +0200

    Variables

---
+++ src/hu/ebamine/tutorial.java
@@ -0,0 +1,15 @@
+package hu.ebamine;
+
+public class tutorial {
+	
+	static int pig = 15;
+	static String sheep = "The answer is:";
+	static double cow = 42.456; 
+	static boolean chicken = true;
+	public static void main(String[] args)
+	
+	{
+		System.out.println(sheep+pig);
+	}
+
+}
