e45367c33f4d33b20dc78a2120b7737de66f00c5
c1gm <63867002+c1gm@users.noreply.github.com>
2020-04-26 19:03:49 +0200

    Update tutorial.java

--- src/hu/ebamine/tutorial.java
+++ src/hu/ebamine/tutorial.java
@@ -2,14 +2,24 @@ package hu.ebamine;
 
 public class tutorial {
 	
-	static int pig = 15;
-	static String sheep = "The answer is:";
+	static int pig = 15-5;
+	static String sheep = "The first if statement is true!" ;
 	static double cow = 42.456; 
 	static boolean chicken = true;
-	public static void main(String[] args)
-	
-	{
-		System.out.println(sheep+pig);
+	public static void main(String[] args){
+		
+		if (pig >= cow) 
+		{
+			System.out.println(sheep);
+		}
+		else if (pig == 11)
+		{
+			System.out.println("The second if statement is true!");
+		}
+		else
+		{
+			System.out.println("None of the if statements are true!");
+		}
+		System.out.println(pig);
 	}
-
 }
